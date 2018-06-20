b2a0230f4530eb0144a4451cc4f42d14ba94b253
Hellen Lee <hleexx@gmail.com>
2018-06-20 00:54:24 -0400

    feat: Enter API Key and scripts for the map visuals

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoiaGxlZXh4IiwiYSI6ImNqaW1pb2RwaTA0a2wza3BsNDNxdm9veGYifQ.DsuDFt2Cs7VOfVbmFsiW7g',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
